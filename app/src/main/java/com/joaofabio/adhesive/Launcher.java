a48a5db286536c5b53447b0373d94045d823efac
2Noob1 <asphalgamer@outlook.pt>
2020-04-28 21:08:35 +0100

    changed Delay to 3s

--- app/src/main/java/com/joaofabio/adhesive/Launcher.java
+++ app/src/main/java/com/joaofabio/adhesive/Launcher.java
@@ -21,7 +21,7 @@ public class Launcher extends AppCompatActivity {
             public void run() {
                 checkNet();
             }
-        },1000);
+        },3000);
     }
 
     protected void checkNet(){
