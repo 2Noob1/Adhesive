55d815e1cbb4047dc5e4d65e8e6c341627426e5f
2Noob1 <asphalgamer@outlook.pt>
2020-04-29 11:30:45 +0100

    Added Register Interface + translations

--- app/src/main/java/com/joaofabio/adhesive/LoginAction.java
+++ app/src/main/java/com/joaofabio/adhesive/LoginAction.java
@@ -2,7 +2,10 @@ package com.joaofabio.adhesive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginAction extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class LoginAction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_action);
+
+        Button donthaveaccountbtn = findViewById(R.id.button5);
+        donthaveaccountbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+                finishAfterTransition();
+            }
+        });
     }
 }
--- app/src/main/java/com/joaofabio/adhesive/Register.java
+++ app/src/main/java/com/joaofabio/adhesive/Register.java
@@ -2,7 +2,10 @@ package com.joaofabio.adhesive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button alreadyhaveaccountbtn = findViewById(R.id.button7);
+        alreadyhaveaccountbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginAction.class));
+                finishAfterTransition();
+            }
+        });
     }
 }
