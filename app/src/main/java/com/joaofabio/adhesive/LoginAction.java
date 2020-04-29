846bcfc4592f9d1b45d24a43cdea15c7e2f37065
2Noob1 <asphalgamer@outlook.pt>
2020-04-29 10:59:25 +0100

    Added Login Screen + translations

---
+++ app/src/main/java/com/joaofabio/adhesive/LoginAction.java
@@ -0,0 +1,14 @@
+package com.joaofabio.adhesive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginAction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_action);
+    }
+}
--- app/src/main/java/com/joaofabio/adhesive/login.java
+++ app/src/main/java/com/joaofabio/adhesive/login.java
@@ -2,13 +2,38 @@ package com.joaofabio.adhesive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
 
+    Intent ActivityTranslateTo = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button Login = findViewById(R.id.button);
+        Button Register = findViewById(R.id.button2);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityTranslateTo = new Intent(getApplicationContext(),LoginAction.class);
+                startActivity(ActivityTranslateTo);
+            }
+        });
+
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityTranslateTo = new Intent(getApplicationContext(),Register.class);
+                startActivity(ActivityTranslateTo);
+            }
+        });
+
     }
 }
