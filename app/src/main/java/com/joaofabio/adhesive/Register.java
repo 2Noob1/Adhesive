7c848e6f140bba0927b5cb11d67c50b08f707148
2Noob1 <asphalgamer@outlook.pt>
2020-04-29 11:56:47 +0100

    Added DatePickerFragment to Register

--- app/src/main/java/com/joaofabio/adhesive/Register.java
+++ app/src/main/java/com/joaofabio/adhesive/Register.java
@@ -1,14 +1,26 @@
 package com.joaofabio.adhesive;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.CollationElementIterator;
+import java.util.Calendar;
 
 public class Register extends AppCompatActivity {
-
+    EditText tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +34,39 @@ public class Register extends AppCompatActivity {
                 finishAfterTransition();
             }
         });
+
+        tv = findViewById(R.id.editText7);
+        tv.setFocusable(false);
+        tv.setClickable(true);
+
     }
+
+    public void toggledatefragmment(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        public EditText tv;
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            tv = getActivity().findViewById(R.id.editText7);
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            tv.setText(day + "/" + (month + 1) + "/" + year);
+        }
+    }
+
 }
