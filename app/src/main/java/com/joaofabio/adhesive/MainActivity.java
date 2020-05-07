3bb01dd85390e72493c1ddafe3418d2d8e6e55b3
fabio6060 <fabiopicadosousa@gmail.com>
2020-05-07 01:31:04 +0100

    slide image

--- app/src/main/java/com/joaofabio/adhesive/MainActivity.java
+++ app/src/main/java/com/joaofabio/adhesive/MainActivity.java
@@ -4,15 +4,27 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    SliderView sliderView;
+    List<ImageSliderModel>imageSliderModelList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageSliderModelList = new ArrayList<>();
+        sliderView = findViewById(R.id.imageSlider);
 
+        imageSliderModelList.add(new ImageSliderModel(R.drawable.a));
+        imageSliderModelList.add(new ImageSliderModel(R.drawable.b));
+        imageSliderModelList.add(new ImageSliderModel(R.drawable.c));
 
+        sliderView.setSliderAdapter(new ImageSliderAdapter(this,imageSliderModelList));
     }
 }
