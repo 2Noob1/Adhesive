20a92dc0f7b9b1e5b188d54ce2d625703bd4667f
fabio6060 <fabiopicadosousa@gmail.com>
2020-05-12 12:32:03 +0100

    icon

--- app/src/main/java/com/joaofabio/adhesive/fragment_contacts.java
+++ app/src/main/java/com/joaofabio/adhesive/fragment_contacts.java
@@ -9,31 +9,42 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class fragment_contacts extends Fragment {
+public class fragment_contacts extends Fragment implements OnMapReadyCallback {
 
+    GoogleMap map;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_contacts, container, false);
+
     }
+
+
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng Adhevise = new LatLng(38.6366261,-9.1078463);
+        map.addMarker(new MarkerOptions().position(Adhevise).title("Adhevise"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Adhevise));
+    }
 }
