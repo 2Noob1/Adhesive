de65e34136a3dab1d84f59ebd59fd8a5fef4d33a
2Noob1 <asphalgamer@outlook.pt>
2020-05-12 13:00:00 +0100

    Added Maps

--- app/src/main/java/com/joaofabio/adhesive/fragment_contacts.java
+++ app/src/main/java/com/joaofabio/adhesive/fragment_contacts.java
@@ -9,31 +9,80 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class fragment_contacts extends Fragment {
+public class fragment_contacts extends Fragment implements OnMapReadyCallback {
 
+    GoogleMap map;
+    MapView mapFragment;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contacts, container, false);//inflates something
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
+
+    }
+
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapFragment = view.findViewById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.onCreate(savedInstanceState);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {//idk
-        super.onViewCreated(view, savedInstanceState);
+    public void onMapReady(GoogleMap googleMap) {
 
+        map = googleMap;
+        LatLng Adhevise = new LatLng(38.636580908361516,-9.105661788968442);
+        map.addMarker(new MarkerOptions().position(Adhevise).title("Adhevise"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Adhevise,15));
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapFragment.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapFragment.onResume();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapFragment.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapFragment.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapFragment.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapFragment.onLowMemory();
+    }
 }
